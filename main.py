7088e438f926e4d9934cb372d596c2fff1cafd17
Vinay <vinaykumarvk.8866@gmail.com>
2020-08-21 20:39:04 +0530

    Retrieval of Data Successful through API

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # coding: utf-8
 import requests
 
-params = {
+def get_Value(loc):
+  __params = {
   'access_key': 'efa59c8305d828986c91c19480a592a5',
-  'query': 'New York'
-}
+  'query': loc
+  }
+  api_result = requests.get('http://api.weatherstack.com/current', __params)
 
-api_result = requests.get('http://api.weatherstack.com/current', params)
+  api_response = api_result.json()
 
-api_response = api_result.json()
+  # print(api_response)
+  # print(f"Humidity:{api_response['current']['temperature']} Temperature:{api_response['current']['humidity']}")
 
-#print(api_response)
+  return [api_response['request']['query'], api_response['current']['humidity'], api_response['current']['temperature']]
 
-print(u'Current temperature in %s is %d℃' % (api_response['location']['name'], api_response['current']['temperature']))
+print(get_Value('India'))
+
+
